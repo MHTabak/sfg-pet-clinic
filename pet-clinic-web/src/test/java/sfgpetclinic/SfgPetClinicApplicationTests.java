cd91b4db0f3c03ad659dd91a3bc388a0d12102f1
MHTabak <michael_tabak@yahoo.com>
2018-10-15 18:13:58 -0700

    Added wro4j plugin and config from Spring Pet Clinic. Closes #24

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,15 +1,7 @@
 package sfgpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class SfgPetClinicApplicationTests {
 
-    @Test
     public void contextLoads() {
     }
 

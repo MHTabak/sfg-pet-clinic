17aee63d338fd5d79188cc565fd5e933f4c4d4f4
MHTabak <michael_tabak@yahoo.com>
2018-11-08 13:45:04 -0800

    Added contact info to Owner. Closes #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,13 +1,40 @@
 package guru.springframework.sfgpetclinic.model;
 
 // Created Lecture 54
-// Lecture 125 Add a Pet
+// Lecture 125 Add Pets, address, city, telephone
 
 import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
@@ -16,4 +43,5 @@ public class Owner extends Person {
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
+
 }

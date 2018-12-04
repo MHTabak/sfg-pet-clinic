f4b5eb2ab99a287cf4ac4b4fbdd21f7e756bf99b
MHTabak <michael_tabak@yahoo.com>
2018-12-03 18:14:16 -0800

    Updated Vet to use JPA. Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -1,9 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
 // Created in Lecture 126
+// Lecture 154 - annotate with @Entity,
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -3,11 +3,19 @@ package guru.springframework.sfgpetclinic.model;
 // Created Lecture 54
 // Lecture 126 - Add Specialty
 // Lecture 130 - Initialize the specialties list
+// Lecture 154 - Add @Entity, @Table. @ManyToMany, and @JoinTable
+
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+               inverseJoinColumns = @JoinColumn(name= "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {

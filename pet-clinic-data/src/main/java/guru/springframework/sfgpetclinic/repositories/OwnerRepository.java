89fe32da3ef100d4bd86d2ddc494749052f60972
MHTabak <michael_tabak@yahoo.com>
2018-12-14 17:43:17 -0800

    Added Spring Data JPA Service. Closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -1,10 +1,13 @@
 package guru.springframework.sfgpetclinic.repositories;
 
 // Created in Lecture  156
+// Lecture 157 Add findByLastName
 
 import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 
 }
